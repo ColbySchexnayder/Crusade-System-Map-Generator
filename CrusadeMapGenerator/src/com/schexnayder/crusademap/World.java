d290d7e6cb6df09a459d1ba210f9851fbd1f0a66
ColbySchexnayder <schexnayderhusser@gmail.com>
2022-10-03 09:36:06 -0700

    Added climates and narrative quirks

--- CrusadeMapGenerator/src/com/schexnayder/crusademap/World.java
+++ CrusadeMapGenerator/src/com/schexnayder/crusademap/World.java
@@ -11,11 +11,87 @@ public class World {
 	Classification classification = Classification.MOON;
 	int nSectors = 0;
 	String designation = "";
+	String climate = "";
+	ArrayList<String> quirks = new ArrayList<String>();
 	ArrayList<World> moons = new ArrayList<World>();
 	Random ran = new Random();
 	public World(int nSectors) {
 		this.nSectors = nSectors;
+		assignDesignation();
+		determineClimate();
+		narrativeQuirks();
+	}
+
+	public void narrativeQuirks() {
+		int rollD3 = ran.nextInt(MapGenerator.DIE/2);
+		int rollD6 = ran.nextInt(MapGenerator.DIE);
+		switch(rollD3) {
+		case 0:
+			switch(rollD6) {
+			case 0: quirks.add("Civil Unrest"); break;
+			case 1: quirks.add("Evacuated Population"); break;
+			case 2: quirks.add("Terraformed Biosphere"); break;
+			case 3: quirks.add("Lost Xeno Relic"); break;
+			case 4: quirks.add("Atmospheric Interference"); break;
+			case 5: quirks.add("Unreliable Power Grid"); break;
+			}
+			break;
+		case 1:
+			switch(rollD6) {
+			case 0: quirks.add("Noxious Atmosphere"); break;
+			case 1: quirks.add("Localized Warp Storms"); break;
+			case 2: quirks.add("Unreliable Maps"); break;
+			case 3: quirks.add("Resettled World"); break;
+			case 4: quirks.add("Magnetic Flux"); break;
+			case 5: quirks.add("Ancient Labyrinths"); break;
+			}
+			break;
+		case 2:
+			switch(rollD6) {
+			case 0: quirks.add("Turbulent Weather"); break;
+			case 1: quirks.add("Tidally Locked"); break;
+			case 2: quirks.add("Historic Battleground"); break;
+			case 3: quirks.add("Unsettling Ambience"); break;
+			case 4: quirks.add("Exterminated Biosphere"); break;
+			case 5: quirks.add("Volatile Tectonics"); break;
+			}
+			break;
+		}
 		
+		if (classification == Classification.LARGE_PLANET && quirks.size()==1) {
+			narrativeQuirks();
+		}
+	}
+	
+	public void determineClimate() {
+		int roll2D3 = 2 + ran.nextInt(MapGenerator.DIE/2) + ran.nextInt(MapGenerator.DIE/2);
+		if (classification == Classification.MOON) {
+			switch (roll2D3) {
+			case 2: climate = "Captured Asteroid"; break;
+			case 3: climate = "Molten"; break;
+			case 4: climate = "Frozen Wastes"; break;
+			case 5: climate = "Molten"; break;
+			case 6: climate = "Toxic Mire"; break;
+			case 7: climate = "Captured Asteroid"; break;
+			case 8: climate = "Artificial"; break;
+			case 9: climate = "Toxic Mire"; break;
+			}
+		}
+		else {
+			switch (roll2D3) {
+			case 2: climate = "Wasteland"; break;
+			case 3: climate = "Desert"; break;
+			case 4: climate = "Artic"; break;
+			case 5: climate = "Oceanic"; break;
+			case 6: climate = "Swamp"; break;
+			case 7: climate = "Wasteland"; break;
+			case 8: climate = "Artificial"; break;
+			case 9: climate = "Tropical"; break;
+			}
+		}
+	}
+	
+	public void assignDesignation() {
 		switch(ran.nextInt(MapGenerator.DIE)) {
 		case 0: designation = "Hive World, Imperialis"; break;
 		case 1: designation = "Shrine World, Imperialis"; break;
@@ -42,11 +118,16 @@ public class World {
 			classification = Classification.LARGE_PLANET;
 		}
 	}
-
+	
 	@Override
 	public String toString() {
 		String s = "";
-		s += classification + "\nSectors: " + nSectors + "\nDesignation: " + designation + "\n";
+		s += classification + "\nSectors: " + nSectors + "\nClimate: " + climate 
+				+ "\nDesignation: " + designation + "\nQuirks:\n";
+		
+		for (String q : quirks) {
+			s += q + "\n";
+		}
 
 		if (!moons.isEmpty()) {
 			s += "\nMOONS:\n\n";
